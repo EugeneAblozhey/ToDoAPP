ed88ac2d94e3d7ae02d5c8ef707c3f1879d22133
Eugene <ablogei@gmail.com>
2021-09-23 20:36:23 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,87 @@
+var input = document.getElementById('inputTask');
+var tasks = document.getElementsByTagName('li');
+var btnAddTask = document.getElementById('btn-add');
+var taskList = document.getElementById('list');
+var spans = document.getElementsByTagName('span');
+var btnInfo = document.getElementById('btn-info');
+var btnCloseInfo = document.querySelector('.button');
+
+
+
+function addNewTask(){
+    var inputContent = input.value;
+    if(!/\S/.test(inputContent)){
+        input.value = '';
+        input.focus();
+        input.style.border = 'solid red';
+    }else{
+        input.value = '';
+        var newLi = document.createElement('li');
+        newLi.innerText = inputContent;
+        var newSpan = document.createElement('span');
+        newSpan.innerText = ' Delete';
+        var currentTime = document.createTextNode(' ' + new Date().toLocaleDateString());
+        taskList.appendChild(newLi).appendChild(newSpan);
+        newLi.appendChild(currentTime);
+        deleteTask();
+        completedTask();
+    }
+}
+
+function counterCompletedTasks(){
+    var counterCompleted = 0;
+    var counterUncomplited = 0;
+    for(let i=0; i<tasks.length; i++){
+        if(tasks[i].style.textDecoration === 'line-through'){
+            counterCompleted++;
+        }else{
+            counterUncomplited++;
+        }
+    }
+    var elemComp = document.querySelector('.counter-comp');
+    var elemUncomp = document.querySelector('.counter-uncomp');
+    elemComp.innerText = counterCompleted;
+    elemUncomp.innerText = counterUncomplited;
+}
+
+function deleteTask(){
+    for(let spanItem of spans){
+        spanItem.onclick = function(){
+            spanItem.parentElement.remove();
+            counterCompletedTasks();
+        }
+    }
+    
+}
+
+function showModalWindow(){
+    var modalWindow = document.querySelector('.modal-window');
+    modalWindow.style.display = 'block';
+}
+
+function closeModalInfo(){
+    var modalWindow = document.querySelector('.modal-window');
+    modalWindow.style.display = 'none';
+}
+
+function completedTask(){
+    for(let i=0; i<tasks.length; i++){
+        tasks[i].onclick = function(){
+            tasks[i].style.textDecoration = 'line-through';
+            counterCompletedTasks();
+        }
+        counterCompletedTasks();
+    }
+}
+
+
+
+btnCloseInfo.onclick = closeModalInfo;
+
+btnInfo.onclick = showModalWindow;
+
+btnAddTask.onclick = addNewTask;
+
+deleteTask();
+completedTask();
+counterCompletedTasks();
